e58849f1b565d911fe31e926e727e6c0a2bb267e
rajtehriyag-crypto <rajtehriyag@gmail.com>
2026-08-10 13:59:49 +0530

    bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+import telebot
+
+TOKEN = 8980536868:AAHjaPCAcer6TCfbfpMqdcTTp_CFvhnNu7w
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Bot Online Hai 🚀")
+
+@bot.message_handler(commands=['ping'])
+def ping(message):
+    bot.reply_to(message, "Pong 🏓")
+
+bot.infinity_polling()
